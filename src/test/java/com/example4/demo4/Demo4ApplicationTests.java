b6c46a18e0adef91beccb457880ab379a8e760f1
kalemaithri <68313898+kalemaithri@users.noreply.github.com>
2022-06-24 12:50:37 +0530

    Update Demo4ApplicationTests.java

--- src/test/java/com/example4/demo4/Demo4ApplicationTests.java
+++ src/test/java/com/example4/demo4/Demo4ApplicationTests.java
@@ -2,7 +2,7 @@ package com.example4.demo4;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//random comment
 @SpringBootTest
 class Demo4ApplicationTests {
 
